7b2b77957f2dcbbbd97cdae00c71bc27ee5bbdde
Ogojoshua905 <joshuaexcellency1@gmail.com>
2024-03-04 10:58:22 +0100

    fix:  Update the assignment.js file with various array operations and variable assignments.

--- assignment.js
+++ assignment.js
@@ -14,7 +14,7 @@ graph[0].innerHTML = '<bold>Sum of all numbers in the array</bold>:'
 let aname = 'JoshDomain';
 let aage = 17;
 
-const arr1 = [12,34,76,123,56,8,2,9888,90,345];
+const arr1 = [12, 34, 76, 123, 56, 8, 2, 9888, 90, 345];
 
 const arr2 = 'Alice, Bob, Charlie, Darlington, Emma, Fiona, George, Hannah, Joy, Julia' .split(' ')
 
@@ -30,7 +30,17 @@ const divTwo = arr1.filter(num => num % 2 === 0)
 
 const greatThanFifty = arr1.filter( num => num > 50)
 
-arr1.sort((x, y )=> y-x)
+const asc = arr1.sort((x, y )=> x - y)
+
+const desc = arr1.sort((x, y )=> y - x)
+
+const even = arr1.every(num => num % 2 === 0)
+
+const indexOf76 = arr1.indexOf(76)
+
+const lname = arr2.sort((a, b)=>b.length-a.length)[0]
+
+const sname = arr2.sort((a, b) => a.length - b.length)
 
 
 console.log('JavaScript Assignment')
@@ -40,3 +50,11 @@ console.log(largArr1)
 console.log(divTwo)
 console.log(smalArr1)
 console.log(greatThanFifty)
+console.log(asc)
+console.log('Are All Numbers Even?:', even)
+console.log('Find the Index Of 76:', indexOf76)
+console.log(desc)
+console.log(arr2.length)
+console.log(arr2.indexOf("Charlie") !== -1)
+console.log('Longest Name:', lname)
+console.log('Shortest Name:', sname)
